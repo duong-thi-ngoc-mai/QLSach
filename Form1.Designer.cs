3567a1ed60a05ff29f54e5496208abfa71c0a7bb
NGOC MAI <NGOC MAI@LAPTOP-67F1561J>
2023-10-02 15:09:56 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace QLSach
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txttim = new System.Windows.Forms.TextBox();
+            this.txtma = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txttieude = new System.Windows.Forms.TextBox();
+            this.txtsoluong = new System.Windows.Forms.TextBox();
+            this.txtgia = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btthem = new System.Windows.Forms.Button();
+            this.btluu = new System.Windows.Forms.Button();
+            this.btxoa = new System.Windows.Forms.Button();
+            this.bttim = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tìm kiếm ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(108, 216);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mã sách";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(108, 271);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tiêu đề";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(433, 271);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Đơn giá";
+            // 
+            // txttim
+            // 
+            this.txttim.BackColor = System.Drawing.Color.Snow;
+            this.txttim.Location = new System.Drawing.Point(112, 63);
+            this.txttim.Multiline = true;
+            this.txttim.Name = "txttim";
+            this.txttim.Size = new System.Drawing.Size(581, 27);
+            this.txttim.TabIndex = 4;
+            // 
+            // txtma
+            // 
+            this.txtma.BackColor = System.Drawing.Color.Snow;
+            this.txtma.Location = new System.Drawing.Point(201, 213);
+            this.txtma.Multiline = true;
+            this.txtma.Name = "txtma";
+            this.txtma.Size = new System.Drawing.Size(167, 27);
+            this.txtma.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(433, 216);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 20);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Số lượng";
+            // 
+            // txttieude
+            // 
+            this.txttieude.BackColor = System.Drawing.Color.Snow;
+            this.txttieude.Location = new System.Drawing.Point(201, 268);
+            this.txttieude.Multiline = true;
+            this.txttieude.Name = "txttieude";
+            this.txttieude.Size = new System.Drawing.Size(167, 27);
+            this.txttieude.TabIndex = 7;
+            // 
+            // txtsoluong
+            // 
+            this.txtsoluong.BackColor = System.Drawing.Color.Snow;
+            this.txtsoluong.Location = new System.Drawing.Point(526, 213);
+            this.txtsoluong.Multiline = true;
+            this.txtsoluong.Name = "txtsoluong";
+            this.txtsoluong.Size = new System.Drawing.Size(167, 27);
+            this.txtsoluong.TabIndex = 8;
+            // 
+            // txtgia
+            // 
+            this.txtgia.BackColor = System.Drawing.Color.Snow;
+            this.txtgia.Location = new System.Drawing.Point(526, 268);
+            this.txtgia.Multiline = true;
+            this.txtgia.Name = "txtgia";
+            this.txtgia.Size = new System.Drawing.Size(167, 27);
+            this.txtgia.TabIndex = 9;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.LightBlue;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(112, 410);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(582, 157);
+            this.dataGridView1.TabIndex = 10;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // btthem
+            // 
+            this.btthem.BackColor = System.Drawing.Color.Snow;
+            this.btthem.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btthem.Location = new System.Drawing.Point(198, 352);
+            this.btthem.Name = "btthem";
+            this.btthem.Size = new System.Drawing.Size(97, 31);
+            this.btthem.TabIndex = 11;
+            this.btthem.Text = "Thêm mới";
+            this.btthem.UseVisualStyleBackColor = false;
+            this.btthem.Click += new System.EventHandler(this.btthem_Click);
+            // 
+            // btluu
+            // 
+            this.btluu.BackColor = System.Drawing.Color.Snow;
+            this.btluu.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btluu.Location = new System.Drawing.Point(357, 352);
+            this.btluu.Name = "btluu";
+            this.btluu.Size = new System.Drawing.Size(102, 31);
+            this.btluu.TabIndex = 12;
+            this.btluu.Text = "Cập nhật";
+            this.btluu.UseVisualStyleBackColor = false;
+            this.btluu.Click += new System.EventHandler(this.btluu_Click);
+            // 
+            // btxoa
+            // 
+            this.btxoa.BackColor = System.Drawing.Color.Snow;
+            this.btxoa.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btxoa.Location = new System.Drawing.Point(530, 352);
+            this.btxoa.Name = "btxoa";
+            this.btxoa.Size = new System.Drawing.Size(75, 31);
+            this.btxoa.TabIndex = 13;
+            this.btxoa.Text = "Xóa";
+            this.btxoa.UseVisualStyleBackColor = false;
+            this.btxoa.Click += new System.EventHandler(this.btxoa_Click);
+            // 
+            // bttim
+            // 
+            this.bttim.BackColor = System.Drawing.Color.LightCoral;
+            this.bttim.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttim.Location = new System.Drawing.Point(699, 61);
+            this.bttim.Name = "bttim";
+            this.bttim.Size = new System.Drawing.Size(49, 29);
+            this.bttim.TabIndex = 14;
+            this.bttim.Text = "=>";
+            this.bttim.UseVisualStyleBackColor = false;
+            this.bttim.Click += new System.EventHandler(this.bttim_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(800, 615);
+            this.Controls.Add(this.bttim);
+            this.Controls.Add(this.btxoa);
+            this.Controls.Add(this.btluu);
+            this.Controls.Add(this.btthem);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtgia);
+            this.Controls.Add(this.txtsoluong);
+            this.Controls.Add(this.txttieude);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtma);
+            this.Controls.Add(this.txttim);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txttim;
+        private System.Windows.Forms.TextBox txtma;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txttieude;
+        private System.Windows.Forms.TextBox txtsoluong;
+        private System.Windows.Forms.TextBox txtgia;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btthem;
+        private System.Windows.Forms.Button btluu;
+        private System.Windows.Forms.Button btxoa;
+        private System.Windows.Forms.Button bttim;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace QLSach
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void load_data()
+        {
+            SqlConnection con = new SqlConnection(" server = LAPTOP-67F1561J\\DUONGNGOCMAI_SQL ; database = Sach; integrated security = true ");
+            SqlDataAdapter da = new SqlDataAdapter(" select * from Sach01", con);
+            DataTable dt = new DataTable();
+            con.Open();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btthem_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(" server = LAPTOP-67F1561J\\DUONGNGOCMAI_SQL ; database = Sach; integrated security = true ");
+            SqlCommand cmd = new SqlCommand ("insert into Sach01 values ('" +txtma.Text+ "', '" +txttieude.Text+ "', '" + Convert.ToDouble(txtgia.Text)+"', '" +Convert.ToInt16(txtsoluong.Text)+"' )", con);
+            con.Open();
+            int ret = cmd.ExecuteNonQuery();
+            if(ret ==1)
+            MessageBox.Show("Them sach thanh cong");
+            con.Close();
+            load_data();
+        }
+
+        private void btluu_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(" server = LAPTOP-67F1561J\\DUONGNGOCMAI_SQL ; database = Sach; integrated security = true ");
+            SqlCommand cmd = new SqlCommand("update Sach01 set tieude = '" + txttieude.Text + "', dongia = '" + Convert.ToDouble(txtgia.Text) + "', soluong = '" + Convert.ToInt16(txtsoluong.Text) + "' where masach = '" + txtma.Text + "'", con);
+            con.Open();
+            int ret = cmd.ExecuteNonQuery();
+            if (ret == 1)
+                MessageBox.Show("Cap nhat thanh cong");
+            con.Close();
+            load_data();
+        }
+
+        private void btxoa_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(" server = LAPTOP-67F1561J\\DUONGNGOCMAI_SQL ; database = Sach; integrated security = true ");
+            SqlCommand cmd = new SqlCommand("delete from Sach01 where masach = '" + txtma.Text + "'", con);
+            con.Open();
+            int ret = cmd.ExecuteNonQuery();
+            if (ret == 1)
+                MessageBox.Show("Xoa Sach thanh cong");
+            con.Close();
+            load_data();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            load_data();
+
+        }
+
+        private void bttim_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(" server = LAPTOP-67F1561J\\DUONGNGOCMAI_SQL ; database = Sach; integrated security = true ");
+            SqlDataAdapter da = new SqlDataAdapter(" select * from Sach01 where masach LIKE '%" + txttim.Text+ "%' ", con);
+            DataTable dt = new DataTable();
+            con.Open();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtma.Text = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+            txttieude.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            txtgia.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+            txtsoluong.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+
+        }
+    }
+}
